3dfe92afc6bc0496eebc813e496fd8969286bbb7
Michael Pavling <pavling@gmail.com>
2013-10-23 11:00:01 +0100

    refactoring method of summing hamming distance

--- hamming.rb
+++ hamming.rb
@@ -5,9 +5,8 @@ class Hamming
     comparison_dna_chars = comparison_dna.chars.to_a
     maximum_iterations = [comparison_dna.size,dna.size].min
 
-    (0..maximum_iterations-1).inject(0) do |memo, index|
-      memo += 1 unless dna_chars[index] == comparison_dna_chars[index]
-      memo
+    (0..maximum_iterations-1).each_with_index.count do |memo, index|
+      dna_chars[index] != comparison_dna_chars[index]
     end
   end
 
