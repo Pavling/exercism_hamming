372e1ce1b4046d9242e1e7ceba971e226b8e565e
Michael Pavling <pavling@gmail.com>
2013-10-23 10:52:59 +0100

    passing test_no_difference_between_empty_strands with code from point_mutations exercise

---
+++ hamming.rb
@@ -0,0 +1,14 @@
+class Hamming
+
+  def self.compute(dna, comparison_dna)
+    dna_chars = dna.chars.to_a
+    comparison_dna_chars = comparison_dna.chars.to_a
+    maximum_iterations = [comparison_dna.size,dna.size].min
+
+    (0..maximum_iterations-1).inject(0) do |memo, index|
+      memo += 1 unless dna_chars[index] == comparison_dna_chars[index]
+      memo
+    end
+  end
+
+end
