86ea9ee94adacb059b0ddd2d16e194ad1b889e81
Michael Pavling <pavling@gmail.com>
2014-11-27 14:56:18 +0000

    tweaking hamming method

--- hamming.rb
+++ hamming.rb
@@ -1,13 +1,10 @@
+require 'pry-byebug'
+
 class Hamming
 
   def self.compute(dna, comparison_dna)
-    dna_chars = dna.chars.to_a
-    comparison_dna_chars = comparison_dna.chars.to_a
-    maximum_iterations = [comparison_dna.size,dna.size].min
-
-    (0..maximum_iterations-1).each_with_index.count do |memo, index|
-      dna_chars[index] != comparison_dna_chars[index]
-    end
+    zipped_dnas = dna.chars.zip(comparison_dna.chars)
+    zipped_dnas.reject { |a,b| b.nil? || a == b }.count
   end
 
 end
